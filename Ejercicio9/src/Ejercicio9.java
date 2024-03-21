413358258f0dd29d5a457472c2c42fd982982146
pablo <pmherrerar01@educarex.es>
2024-03-21 20:59:27 +0100

    Ejercicio9 por terminar

--- Ejercicio9/src/Ejercicio9.java
+++ Ejercicio9/src/Ejercicio9.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
 
 public class Ejercicio9 {
-
-	public static int pedirNumero() {
-		Scanner entrada = new Scanner(System.in);
-		int num;
-		System.out.println("Introduce un numero");
-		num = entrada.nextInt();
-		return num;
-	}
-	
-	public static void ordenar(int num) {
-		int mayor, aux, num1, num2, num3, num4;
-		num1=pedirNumero();
-		num2=pedirNumero();
-		num3=pedirNumero();
-		num4=pedirNumero();
-		if(num1>num2&&num1>num3&&){
-			
-		}
-		
-		
-	}
 	
 	public static void main(String[] args) {
-		
+		int num1, num2, num3, num4, mayor=0;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		num1 = entrada.nextInt();
+		Scanner entrada2 = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		num2 = entrada2.nextInt();
+		Scanner entrada3 = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		num3 = entrada3.nextInt();
+		Scanner entrada4 = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		num4 = entrada4.nextInt();
+		if(num1>num2){
+		mayor = num1;
+		System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
+		}else {
+			if(num1>num3) {
+				mayor=num1;
+				System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
+			}else {
+				if(num1>num4) {
+					mayor=num1;
+					System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
+				}
+			}
 		}
+		
+		
+	}
 
 }
