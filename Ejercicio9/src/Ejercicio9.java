7b5e81f38da8a6b727d35368abdb65ff7269064f
pablo <pmherrerar01@educarex.es>
2024-03-21 12:42:09 +0100

    Ejercicio9 por terminar

---
+++ Ejercicio9/src/Ejercicio9.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static int pedirNumero() {
+		Scanner entrada = new Scanner(System.in);
+		int num;
+		System.out.println("Introduce un numero");
+		num = entrada.nextInt();
+		return num;
+	}
+	
+	public static void ordenar(int num) {
+		int mayor, aux, num1, num2, num3, num4;
+		num1=pedirNumero();
+		num2=pedirNumero();
+		num3=pedirNumero();
+		num4=pedirNumero();
+		if(num1>num2&&num1>num3&&){
+			
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		}
+
+}
