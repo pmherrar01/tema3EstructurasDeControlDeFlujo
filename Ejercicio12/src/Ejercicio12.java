b220777387e7fbdd666530a1846089f57b180272
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2024-03-24 19:21:18 +0100

    Ejercicio12 terminado

---
+++ Ejercicio12/src/Ejercicio12.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio12 {
+	
+	public static void main(String[] args) {
+		int cont = 11;
+		do {
+			if((cont/2)==0) {
+				System.out.println("par:" + cont);
+				cont++;
+			}
+		}while(cont<=133);
+
+
+	}
+
+}
