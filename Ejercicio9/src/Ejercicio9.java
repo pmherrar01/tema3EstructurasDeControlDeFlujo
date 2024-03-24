fd14f9e8120e0c5ca98dbd2d8230e82bb6d506d5
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2024-03-24 18:53:54 +0100

    Ejercicio9 terminado

--- Ejercicio9/src/Ejercicio9.java
+++ Ejercicio9/src/Ejercicio9.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Ejercicio9 {
-	
+
 	public static void main(String[] args) {
-		int num1, num2, num3, num4, mayor=0;
+		int num1, num2, num3, num4, aux;
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Introduce un numero");
 		num1 = entrada.nextInt();
@@ -16,22 +16,27 @@ public class Ejercicio9 {
 		Scanner entrada4 = new Scanner(System.in);
 		System.out.println("Introduce un numero");
 		num4 = entrada4.nextInt();
-		if(num1>num2){
-		mayor = num1;
-		System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
-		}else {
-			if(num1>num3) {
-				mayor=num1;
-				System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
-			}else {
-				if(num1>num4) {
-					mayor=num1;
-					System.out.println("El numero mayor es el:" + mayor + "luego " + num2 + num3 + num4);
-				}
+
+		for(int i = 0;i < 3;i++){
+			if (num1 > num2){
+				aux = num1;
+				num1 = num2;
+				num2 = aux;
 			}
+			if (num2 > num3){
+				aux = num2;
+				num2 = num3;
+				num3 = aux;
+			}
+			if (num3 > num4){
+				aux = num3;
+				num3 = num4;
+				num4 = aux;
+			}    
 		}
-		
-		
+
+		System.out.print("El orden de los números introducidos es: "+num1+" - "+num2+" - "+num3+" - "+num4);
+
 	}
 
 }
