a938fe2c71fd01e23ba43d37ccf4d066ffeef569
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2024-03-24 19:01:08 +0100

    Ejercicio11 terminado

---
+++ Ejercicio11/src/Ejercicio11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<7;i++) {
+			System.out.print("Hola" + i+ "-");
+		}			
+
+	}
+
+}
